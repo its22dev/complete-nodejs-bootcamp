ffb6e208a757f0a40b4ce732d3a4999a90910b9b
its.22.dev <its.22.dev@gmail.com>
2023-06-28 13:41:32 +0800

    044: async/await

---
+++ 3-asynchronous-JS/index4.js
@@ -0,0 +1,42 @@
+// jonasschmedtmann node.js
+// 44
+
+const fs = require('fs');
+const superagent = require('superagent');
+
+const readFilePro = (file) => {
+  return new Promise((resolve, reject) => {
+    fs.readFile(file, (err, data) => {
+      if (err) reject('Error for Reading File');
+      resolve(data);
+    })
+  })
+}
+
+const writeFilePro = (file, data) => {
+  return new Promise((resolve, reject) => {
+    fs.writeFile(file, data, err => {
+      if (err) reject('Error for Writing File')
+      resolve('Success for Writing File')
+    })
+  })
+}
+
+
+const getDogPic = async () => {
+  try {
+    const data = await readFilePro(`${__dirname}/dog.txt`);
+    console.log('Hi,' + data);
+
+    const results = await superagent.get(`https://dog.ceo/api/breed/${data}/images/random`);
+    console.log(results.body.message);
+
+    await writeFilePro('dog-img.txt', results.body.message)
+    console.log('-----Success-----');
+
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+getDogPic();
